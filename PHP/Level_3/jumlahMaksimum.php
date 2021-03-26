18ade620a5eef52bf4fcc1d2db18aeecd5eea22e
noVAL <dennnoval@gmail.com>
2021-03-26 14:10:37 +0000

    Update jumlahMaksimum.php

--- PHP/Level_3/jumlahMaksimum.php
+++ PHP/Level_3/jumlahMaksimum.php
@@ -2,45 +2,46 @@
 
 # https://kotakode.com/challenges/details/3/7272/Jalur-Jumlah-Maksimum
 
-function jumlahMaksimum($arr1,$arr2) {
-  # Mengurutkan secara ascending parameter arr1 dan arr2
+function jumlahMaksimum($arr1, $arr2) {
   sort($arr1); sort($arr2);
-  # Inisialisasi variabel a dengan nilai dari parameter arr1
-  $a = $arr1;
-  # Inisialisasi variabel b dengan nilai dari parameter arr2
-  $b = $arr2;
-  # Inisialisasi variabel l dengan hasil pengecekan antara apakah panjang array a lebih besar b
-  $l = count($a) > count($b) ? count($a) : count($b);
-  # Inisialisasi variabel t dengan nilai 0 untuk menampung total nilai yg dipilih
-  $t = 0;
-  # Inisialisasi variabel s dengan nilai false sebagai state untuk swap elemen antara array a dan b
+  $a = $arr1; $b = $arr2;
+  
+  $c = null;
+  
+  # Pemilihan array yang cocok sebagai array default
+  if (count($b) < count($b))
+    if (array_sum($b) < array_sum($a))
+      $c = $b;
+    else $c = $a;
+  else if (count($b) > count($a))
+    if (array_sum($b) < array_sum($a))
+      $c = $a;
+    else $c = $b;
+  else
+    if (array_sum($b) < array_sum($a))
+      $c = $b;
+    else $c = $a;
+      
+  # Pemilihan array yang cocok sebagai array pembanding
+  $d = $c==$a ? $b : $a;
+      
+  $l = count($c) >= count($d) ? count($c) : count($d);
+  
+  # Inisialisasi array kosong untuk menampung hasil seleksi pada looping
+  $t = [];
+  
   $s = false;
-  # Inisialisasi variabel i dengan nilai 0 untuk indeks looping
-  $i = 0;
-  # Looping selama nilai i kurang dari nilai l
-  while ($i < $l) {
-    # Inisialisasi variabel c dengan nilai dari hasil pengecekan apakah panjang array a lebih kecil dari array b
-    $c = count($a) < count($b) ? $a[$i] : $b[$i];
-    # Pengecekan apakah nilai elemen ke-i pada array b sama dengan nilai elemen indeks ke-i pada array a
-    if ($b[$i]==$a[$i]) 
-      # Ubah nilai s menjadi true
-      $s = true;
-    # Lakukan pengecekan jika nilai s sama dengan true
+  
+  for ($i=0; $i<$l; $i++) {
+    for ($j=0; $j<$l; $j++) {
+    	if ($c[$i]==$d[$j] && $i!=$j) {
+    		$s = true;
+    		array_push($t,($c[$i]+$d[$j])/2);
+    	}
+    }
     if ($s)
-      # Pengecekan apakah nilai variabel c sama dengan nilai elemen ke-i pada array b
-      if ($c==$b[$i]) 
-        # Ubah nilai variabel c dengan nilai dari elemen ke-i pada array a
-        $c = $a[$i];
-      # Jika pengecekan diatas salah maka
-      else
-        # Ubah nilai variabel c dengan nilai dari elemen ke-i pada array b
-        $c = $b[$i];
-    # Tambahkan nilai variabel t dengan nilai dari variabel c
-    $t += $c;
-    # Tambahkan nilai i dengan nilai 1 untuk proses looping
-    $i += 1;
+		  array_push($t,$d[$i]);
+    else array_push($t,$c[$i]);
   }
-  # Simpan nilai variabel t ke dalam variabel output
-  $output = $t;
-  return $output;
+  return $t;
 }
